be9794ae59ada9feccc59d4d4b1f93e23b110114
qualitatshausschweiz-create <qualitatshausschweiz@gmail.com>
2026-01-29 22:51:40 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
+const archivio = document.getElementById("archivio");
 
+function aggiungiVoce(titolo, descrizione, link) {
+  const voce = document.createElement("div");
+  voce.innerHTML = `
+    <strong>${titolo}</strong><br />
+    <em>${descrizione}</em><br />
+    <a href="${link}" target="_blank">Apri su GitHub</a>
+  `;
+  archivio.appendChild(voce);
+}
+
+// Esempio iniziale
+aggiungiVoce(
+  "Note App",
+  "Versione ufficiale con categorie, backup e popup iOS",
+  "https://github.com/tuo-utente/NoteApp"
+);
